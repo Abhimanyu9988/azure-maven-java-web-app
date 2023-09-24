622b8aad5b7c099870dc961a4b12e6f1043bd6b1
Abhi Bajaj <abhi.bajaj@appdynamics.com>
2023-09-24 22:55:42 +0200

    README.md

--- deployapp.sh
+++ deployapp.sh
@@ -2,5 +2,6 @@ echo "Let's deploy the app"
 mkdir helloworld
 cd helloworld
 mvn archetype:generate "-DgroupId=example.demo" "-DartifactId=helloworld" "-DarchetypeArtifactId=maven-archetype-webapp" "-DarchetypeVersion=1.4" "-Dversion=1.0-SNAPSHOT"
+sleep 60
 mvn com.microsoft.azure:azure-webapp-maven-plugin:2.5.0:config
 mvn package azure-webapp:deploy
