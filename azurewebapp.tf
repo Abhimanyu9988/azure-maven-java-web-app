f6867057d0ddb4e47b3a26ea8cb41c83e2c92388
Abhi Bajaj <abhi.bajaj@appdynamics.com>
2023-09-24 20:37:24 +0200

    README.md

---
+++ azurewebapp.tf
@@ -0,0 +1,54 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "3.74.0"
+    }
+  }
+}
+
+###If you don't specify provider configuration, TF will use CLI as the default provider
+###If you want to use a different provider, you can specify it in the provider block
+
+provider "azurerm" {
+  features {}
+}
+
+####Create a resource group
+resource "azurerm_resource_group" "rg" {
+  name     = "abhi-arg-tf"
+  location = "East US"
+}
+
+
+####Create a service plan. 
+####asp => azurerm_service_plan 
+resource "azurerm_service_plan" "asp" {
+  name                = "abhi-asp-tf"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  os_type             = "Linux"
+  sku_name = "P1v3"
+}
+
+####Create a web app
+####alwebapp=azurerm_linux_web_app
+resource "azurerm_linux_web_app" "linux_webapp" {
+  name                = "abhi-java-webapp-tf"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  service_plan_id = azurerm_service_plan.asp.id
+  public_network_access_enabled = true
+  tags = {
+    owner = "Abhi Bajaj"
+  }
+
+  site_config {
+    application_stack {
+      java_version = 8
+      java_server = "JBOSSEAP"
+      java_server_version = "7"
+      #To list all the available stacks, run az webapp list-runtimes --linux
+    }
+  }
+}
