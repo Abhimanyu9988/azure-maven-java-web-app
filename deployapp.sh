66d61037ead6b2107ca3f2244b17d621310e40fa
Abhi Bajaj <abhi.bajaj@appdynamics.com>
2023-09-24 22:49:21 +0200

    README.md

--- deployapp.sh
+++ deployapp.sh
@@ -3,4 +3,4 @@ mkdir helloworld
 cd helloworld
 mvn archetype:generate "-DgroupId=example.demo" "-DartifactId=helloworld" "-DarchetypeArtifactId=maven-archetype-webapp" "-DarchetypeVersion=1.4" "-Dversion=1.0-SNAPSHOT"
 mvn com.microsoft.azure:azure-webapp-maven-plugin:2.5.0:config
-mvn package azure-webapp:deploy  
+mvn package azure-webapp:deploy
--- deploytf.sh
+++ deploytf.sh
@@ -1,6 +1,4 @@
 echo "Let's deploy the terraform"
 sleep 2
-curl -O -L https://github.com/Abhimanyu9988/azure-maven-java-web-app/raw/main/azurewebapp.tf
-curl -O -L https://github.com/Abhimanyu9988/azure-maven-java-web-app/raw/main/variables.tf
 terraform init
 terraform apply
