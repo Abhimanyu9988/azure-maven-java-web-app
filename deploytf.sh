b85ff3bb8881f8160a00c231d9d8f04acb0132e8
Abhi Bajaj <abhi.bajaj@appdynamics.com>
2023-09-24 22:29:27 +0200

    README.md

--- deploytf.sh
+++ deploytf.sh
@@ -3,4 +3,4 @@ sleep 2
 curl -O -L https://github.com/Abhimanyu9988/azure-maven-java-web-app/raw/main/azurewebapp.tf
 curl -O -L https://github.com/Abhimanyu9988/azure-maven-java-web-app/raw/main/variables.tf
 terraform init
-terraform apply -auto-approve
+terraform apply
